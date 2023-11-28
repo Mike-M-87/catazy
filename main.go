ae28547880cc198bb3b3aadc8e16571d0c5991a7
joe <micminn87@gmail.com>
2023-11-28 14:51:37 +0300

    fix all regexes

--- main.go
+++ main.go
@@ -4,22 +4,28 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"regexp"
+	"strconv"
 )
 
 type Noty struct {
-	Quality  string
-	Title    string
-	Year     string
-	Image    string
-	FilmType string
-	WatchUrl string
-	Duration string
+	Quality           string
+	Title             string
+	YearOrSeason      string
+	Image             string
+	FilmType          string
+	WatchUrl          string
+	DurationOrEpisode string
 }
 
 func main() {
-	Catazz()
+	films, err := Catazz()
+	if err != nil {
+		log.Fatal(err)
+	}
+	println(AsPrettyJson(films))
 }
 
 func Catazz() ([]Noty, error) {
@@ -38,41 +44,76 @@ func Catazz() ([]Noty, error) {
 	titleRe := regexp.MustCompile(`<h3 class="film-name"><a\s+[^>]+>([^<]+)</a>\s+</h3>`)
 	qualityRe := regexp.MustCompile(`<div class="pick film-poster-quality">(.*?)</div>`)
 	imgRe := regexp.MustCompile(`<img data-src="([^"]+)"`)
+
 	yearRe := regexp.MustCompile(`<span class="fdi-item">(\d{4})</span>`)
+	tvSeasonRe := regexp.MustCompile(`<span class="fdi-item">(.*?)</span>`)
+
 	typeRe := regexp.MustCompile(`<span class="float-right fdi-type">([^<]+)</span>`)
-	minsRe := regexp.MustCompile(`<span class="fdi-item fdi-duration">([^<]+)</span>`)
+	minsRe := regexp.MustCompile(`<span class="fdi-item fdi-duration">(\d+)m</span>`)
+	tvEpisodeRe := regexp.MustCompile(`<span class="fdi-item">([\d.]+)</span>`)
+
 	urlRe := regexp.MustCompile(`<a\s+href="([^"]+)"\s+class="[^"]+"\s+`)
 
-	var noties []Noty
-	fmt.Println("🟡🟡🟡🟡🟡🟡🟡")
+	noties := make([]Noty, len(notIds))
 
-	for _, v := range notIds {
-		fmt.Print()
+	for i, v := range notIds {
+		filmType := typeRe.FindStringSubmatch(v)
+		var duration []string
+		var yearOrSeason []string
+		var minutes string
+		var year string
+
+		if len(filmType) > 1 && filmType[1] == "TV" {
+			duration = tvEpisodeRe.FindStringSubmatch(v)
+			if len(duration) > 1 {
+				minutes = "EP " + duration[1]
+			}
+			yearOrSeason = tvSeasonRe.FindStringSubmatch(v)
+			if len(yearOrSeason) > 1 {
+				year =  yearOrSeason[1]
+			}
+		} else {
+			duration = minsRe.FindStringSubmatch(v)
+			if len(duration) > 1 {
+				durationStr := duration[1]
+				durationMinutes, _ := strconv.Atoi(durationStr)
+				hours := durationMinutes / 60
+				mins := durationMinutes % 60
+				minutes = fmt.Sprintf("%dh %dm", hours, mins)
+			}
+			yearOrSeason = yearRe.FindStringSubmatch(v)
+			if len(yearOrSeason) > 1 {
+				year = yearOrSeason[1]
+			}
+		}
+
 		fpq := qualityRe.FindStringSubmatch(v)
 		tt := titleRe.FindStringSubmatch(v)
 		img := imgRe.FindStringSubmatch(v)
-		year := yearRe.FindStringSubmatch(v)
-		minutes := minsRe.FindStringSubmatch(v)
-		filmType := typeRe.FindStringSubmatch(v)
 		watchurl := urlRe.FindStringSubmatch(v)
 
 		var noty Noty
 
-		if len(fpq) > 1 && len(tt) > 1 && len(img) > 1 && len(year) > 1 && len(filmType) > 1 && len(minutes) > 1 && len(watchurl) > 1 {
+		if len(fpq) > 1 {
 			noty.Quality = fpq[1]
-			noty.Title = tt[1]
-			noty.Duration = minutes[1]
-			noty.Image = img[1]
-			noty.FilmType = filmType[1]
-			noty.WatchUrl = watchurl[1]
-			noty.Year = year[1]
-			noties = append(noties, noty)
-		} else {
-			fmt.Println(v, "❇️", fpq, "❎", tt, "❎", img, "❎", year, "❎", minutes, "❎", filmType, "❎", watchurl, "❎", "❌❌❌\n\n")
 		}
-	}
+		if len(tt) > 1 {
+			noty.Title = tt[1]
+		}
+		if len(img) > 1 {
+			noty.Image = img[1]
+		}
+		if len(filmType) > 1 {
+			noty.FilmType = filmType[1]
+		}
+		noty.DurationOrEpisode = minutes
+		noty.YearOrSeason = year
 
-	fmt.Println(len(noties), len(notIds))
+		if len(watchurl) > 1 {
+			noty.WatchUrl = "https://cataz.to/" + watchurl[1]
+		}
+		noties[i] = noty
+	}
 	return noties, nil
 }
 
